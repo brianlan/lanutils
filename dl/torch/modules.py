f643bb87905616614633503269d0bc9e249024e2
brianlan <brianlanbo@gmail.com>
2018-11-19 15:50:47 +0800

    generalized the UpperBoundedReLU to more general class which supports setting CAPs to both upper and lower.

--- dl/torch/modules.py
+++ dl/torch/modules.py
@@ -1,22 +1,21 @@
+from collections import namedtuple
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class UpperBoundedReLU(nn.Hardtanh):
-    _FLOAT_MAX = 999999999.
+Boundary = namedtuple('Boundary', 'lower upper')
 
-    def __init__(self, momentum=0.1, max_val=None, track_running_stats=True, estimation_function=None):
-        super(UpperBoundedReLU, self).__init__()
-        self.max_val = max_val
-        self.momentum = momentum
-        self.track_running_stats = track_running_stats
-        self.upperbound_activated = False
-        self._estimated_upperbound = None
 
-        if estimation_function is None:
-            self.estimation_function = lambda miu, std: abs(miu) + abs(std)
+class StatefulHardtanh(nn.Hardtanh):
+    default_boundary = Boundary(-999999999., 999999999.)
 
+    @staticmethod
+    def _default_estimation_function():
+        return lambda miu, std: (-(abs(miu) + abs(std)), abs(miu) + abs(std))
+
+    def _register_tracking_state_attrs(self):
         if self.track_running_stats:
             self.register_buffer('running_mean', torch.tensor(0.))
             self.register_buffer('running_std', torch.tensor(0.))
@@ -26,26 +25,57 @@ class UpperBoundedReLU(nn.Hardtanh):
             self.register_parameter('running_std', None)
             self.register_parameter('num_batches_tracked', None)
 
-    def activate_upperbound(self):
-        self.upperbound_activated = True
+    def __init__(self, momentum=0.1, enforced_min=None, enforced_max=None, track_running_stats=True,
+                 estimation_function=None):
+        super(StatefulHardtanh, self).__init__()
+        self.enforced_min = enforced_min
+        self.enforced_max = enforced_max
+        self.momentum = momentum
+        self.track_running_stats = track_running_stats
+        self.boundary_activated = False
+        self._estimated_boundary = None
+        self.estimation_function = estimation_function or self._default_estimation_function()
+        self._register_tracking_state_attrs()
+
+    def activate_boundary(self):
+        self.boundary_activated = True
         return self
 
-    def deactivate_upperbound(self):
-        self.upperbound_activated = False
+    def deactivate_boundary(self):
+        self.boundary_activated = False
         return self
 
     @property
-    def estimated_upperbound(self):
-        if self.training or self._estimated_upperbound is None:
+    def estimated_boundary(self):
+        if self.training or self._estimated_boundary is None:
             if self.running_mean is not None and self.running_std is not None:
-                self._estimated_upperbound = self.estimation_function(self.running_mean, self.running_std)
-        return self._estimated_upperbound
+                self._estimated_boundary = Boundary(*self.estimation_function(self.running_mean, self.running_std))
+        return self._estimated_boundary
+
+    def _get_boundary_by_priority(self):
+        boundary = self.default_boundary
+        if self.estimated_boundary is not None:
+            boundary = Boundary(self.estimated_boundary.lower, self.estimated_boundary.upper)
+        boundary = Boundary(boundary.lower if self.enforced_min is None else self.enforced_min,
+                            boundary.upper if self.enforced_max is None else self.enforced_max)
+        return boundary
+
+    @staticmethod
+    def _hardtanh(input, boundary):
+        return F.hardtanh(input, boundary.lower, boundary.upper, False)
 
     def forward(self, input):
         if self.training and self.track_running_stats:
             self.running_mean = self.running_mean * (1 - self.momentum) + self.momentum * input.mean()
             self.running_std = self.running_std * (1 - self.momentum) + self.momentum * input.std()
-        if self.upperbound_activated:
-            upperbound = self.max_val or self.estimated_upperbound or self._FLOAT_MAX
-            return F.hardtanh(input, 0, upperbound, False)
-        return F.relu(input, False)
+        if self.boundary_activated:
+            return self._hardtanh(input, self._get_boundary_by_priority())
+        return self._hardtanh(input, self.default_boundary)
+
+
+class StatefulClippedReLU(StatefulHardtanh):
+    def __init__(self, momentum=0.1, clip_at=None, track_running_stats=True, estimation_function=None):
+        super(StatefulClippedReLU, self).__init__(momentum=momentum, enforced_min=0, enforced_max=clip_at,
+                                                  track_running_stats=track_running_stats,
+                                                  estimation_function=estimation_function)
+        StatefulClippedReLU.default_boundary = Boundary(0., self.default_boundary.upper)
--- dl/torch/test/test_modules.py
+++ dl/torch/test/test_modules.py
@@ -2,36 +2,133 @@ import pytest
 import torch
 import numpy as np
 
-from ..modules import UpperBoundedReLU
+from ..modules import StatefulClippedReLU, StatefulHardtanh
 
 
-def test_upper_bounded_relu_normal():
+def test_stateful_hardtanh_normal():
+    input_data = torch.tensor([[-10.1, 1.2], [-0.5, 15.5]], dtype=torch.float32)
+    htanh = StatefulHardtanh()
+    np.testing.assert_almost_equal(htanh.train()(input_data).numpy(),
+                                   np.array([[-10.1, 1.2], [-0.5, 15.5]]), decimal=6)
+    np.testing.assert_almost_equal(htanh.eval()(input_data).numpy(),
+                                   np.array([[-10.1, 1.2], [-0.5, 15.5]]), decimal=6)
+
+
+def test_stateful_hardtanh_min_max_val():
     input_data = torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32)
-    relu = UpperBoundedReLU()
-    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
-    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
+    htanh = StatefulHardtanh(enforced_min=-0.8, enforced_max=1.3)
+    np.testing.assert_almost_equal(htanh.train()(input_data).numpy(), np.array([[-1.1, 1.2], [-0.5, 1.5]]))
+    np.testing.assert_almost_equal(htanh.eval()(input_data).numpy(), np.array([[-1.1, 1.2], [-0.5, 1.5]]))
+    htanh.activate_boundary()
+    np.testing.assert_almost_equal(htanh.train()(input_data).numpy(), np.array([[-0.8, 1.2], [-0.5, 1.3]]))
+    np.testing.assert_almost_equal(htanh.eval()(input_data).numpy(), np.array([[-0.8, 1.2], [-0.5, 1.3]]))
+    htanh.deactivate_boundary()
+    np.testing.assert_almost_equal(htanh.train()(input_data).numpy(), np.array([[-1.1, 1.2], [-0.5, 1.5]]))
+    np.testing.assert_almost_equal(htanh.eval()(input_data).numpy(), np.array([[-1.1, 1.2], [-0.5, 1.5]]))
 
 
-def test_upper_bounded_relu_max_val():
-    input_data = torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32)
-    relu = UpperBoundedReLU(max_val=1.3)
-    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
-    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
-    relu.activate_upperbound()
-    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.3]]))
-    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.3]]))
-    relu.deactivate_upperbound()
-    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
-    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
-
-
-def test_upper_bounded_relu_track_running_mean_std():
+def test_stateful_hardtanh_track_running_mean_std():
     momentum = 0.1
     input_data = [torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32),
                   torch.tensor([[0, 0.2, -0.5, 0.5]], dtype=torch.float32),
                   torch.tensor([-2.1, -0.2, 0.0, -1.1], dtype=torch.float32)]
 
-    relu = UpperBoundedReLU(momentum=momentum).train()
+    htanh = StatefulHardtanh(momentum=momentum).train()
+
+    # Round 1
+    out = htanh(input_data[0])
+    np.testing.assert_almost_equal(out.numpy(), np.array([[-1.1, 1.2], [-0.5, 1.5]]))
+    np.testing.assert_almost_equal(htanh.running_mean.item(), 0.027500002)
+    np.testing.assert_almost_equal(htanh.running_std.item(), 0.127115443)
+
+    # Round 2
+    out = htanh(input_data[1])
+    np.testing.assert_almost_equal(out.numpy(), np.array([[0, 0.2, -0.5, 0.5]]))
+    np.testing.assert_almost_equal(htanh.running_mean.item(), 0.029750001)
+    np.testing.assert_almost_equal(htanh.running_std.item(), 0.156435624)
+
+    # Round 3
+    out = htanh(input_data[2])
+    np.testing.assert_almost_equal(out.numpy(), np.array([-2.1, -0.2, 0.0, -1.1]))
+    np.testing.assert_almost_equal(htanh.running_mean.item(), -0.058224998)
+    np.testing.assert_almost_equal(htanh.running_std.item(), 0.236882284)
+
+    another_data = torch.tensor([[1.1, 1.09], [-0.5, 0.2]], dtype=torch.float32)
+    htanh.eval().activate_boundary()
+    np.testing.assert_almost_equal(htanh(another_data).numpy(),
+                                   np.array([[0.295107282, 0.295107282], [-0.295107282, 0.2]]))
+
+    htanh.train()
+    out = htanh(another_data)
+    np.testing.assert_almost_equal(htanh.running_mean.item(), -0.005152494)
+    np.testing.assert_almost_equal(htanh.running_std.item(), 0.290547669)
+    np.testing.assert_almost_equal(out.numpy(), np.array([[0.295700163, 0.295700163], [-0.295700163, 0.2]]))
+
+    htanh.eval()
+    htanh.enforced_max = 1.0
+    htanh.enforced_min = 0
+    np.testing.assert_almost_equal(htanh(another_data).numpy(), np.array([[1.0, 1.0], [0, 0.2]]))
+
+    htanh.deactivate_boundary()
+    np.testing.assert_almost_equal(htanh(another_data).numpy(), np.array([[1.1, 1.09], [-0.5, 0.2]]))
+
+
+def test_stateful_hardtanh_do_not_track():
+    input_data = [torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32),
+                  torch.tensor([[0, 0.2, -0.5, 0.5]], dtype=torch.float32),
+                  torch.tensor([-2.1, 0.2, 0.5, -1.1], dtype=torch.float32)]
+
+    htanh = StatefulHardtanh(track_running_stats=False).train()
+
+    # Round 1
+    out = htanh(input_data[0])
+    np.testing.assert_almost_equal(out.numpy(), np.array([[-1.1, 1.2], [-0.5, 1.5]]))
+    assert htanh.running_mean is None
+    assert htanh.running_std is None
+
+    # Round 2
+    htanh.eval().activate_boundary()
+    out = htanh(input_data[1])
+    np.testing.assert_almost_equal(out.numpy(), np.array([[0, 0.2, -0.5, 0.5]]))
+    assert htanh.running_mean is None
+    assert htanh.running_std is None
+
+    # Round 3
+    htanh.enforced_max = 0.3
+    htanh.enforced_min = 0.15
+    out = htanh(input_data[2])
+    np.testing.assert_almost_equal(out.numpy(), np.array([0.15, 0.2, 0.3, 0.15]))
+    assert htanh.running_mean is None
+    assert htanh.running_std is None
+
+
+def test_stateful_clipped_relu_normal():
+    input_data = torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32)
+    relu = StatefulClippedReLU()
+    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
+    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
+
+
+def test_stateful_clipped_relu_max_val():
+    input_data = torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32)
+    relu = StatefulClippedReLU(clip_at=1.3)
+    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
+    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
+    relu.activate_boundary()
+    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.3]]))
+    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.3]]))
+    relu.deactivate_boundary()
+    np.testing.assert_almost_equal(relu.train()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
+    np.testing.assert_almost_equal(relu.eval()(input_data).numpy(), np.array([[0, 1.2], [0, 1.5]]))
+
+
+def test_stateful_clipped_relu_track_running_mean_std():
+    momentum = 0.1
+    input_data = [torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32),
+                  torch.tensor([[0, 0.2, -0.5, 0.5]], dtype=torch.float32),
+                  torch.tensor([-2.1, -0.2, 0.0, -1.1], dtype=torch.float32)]
+
+    relu = StatefulClippedReLU(momentum=momentum).train()
 
     # Round 1
     out = relu(input_data[0])
@@ -54,7 +151,7 @@ def test_upper_bounded_relu_track_running_mean_std():
     np.testing.assert_almost_equal(relu.running_std.item(), 0.236882284)
 
     another_data = torch.tensor([[1.1, 1.09], [-0.5, 0.2]], dtype=torch.float32)
-    relu.eval().activate_upperbound()
+    relu.eval().activate_boundary()
     np.testing.assert_almost_equal(relu(another_data).numpy(), np.array([[0.295107282, 0.295107282], [0, 0.2]]))
 
     relu.train()
@@ -64,19 +161,19 @@ def test_upper_bounded_relu_track_running_mean_std():
     np.testing.assert_almost_equal(out.numpy(), np.array([[0.295700163, 0.295700163], [0, 0.2]]))
 
     relu.eval()
-    relu.max_val = 1.0
+    relu.enforced_max = 1.0
     np.testing.assert_almost_equal(relu(another_data).numpy(), np.array([[1.0, 1.0], [0, 0.2]]))
 
-    relu.deactivate_upperbound()
+    relu.deactivate_boundary()
     np.testing.assert_almost_equal(relu(another_data).numpy(), np.array([[1.1, 1.09], [0, 0.2]]))
 
 
-def test_upper_bounded_relu_do_not_track():
+def test_stateful_clipped_relu_do_not_track():
     input_data = [torch.tensor([[-1.1, 1.2], [-0.5, 1.5]], dtype=torch.float32),
                   torch.tensor([[0, 0.2, -0.5, 0.5]], dtype=torch.float32),
                   torch.tensor([-2.1, -0.2, 0.0, -1.1], dtype=torch.float32)]
 
-    relu = UpperBoundedReLU(track_running_stats=False).train()
+    relu = StatefulClippedReLU(track_running_stats=False).train()
 
     # Round 1
     out = relu(input_data[0])
@@ -85,14 +182,14 @@ def test_upper_bounded_relu_do_not_track():
     assert relu.running_std is None
 
     # Round 2
-    relu.eval().activate_upperbound()
+    relu.eval().activate_boundary()
     out = relu(input_data[1])
     np.testing.assert_almost_equal(out.numpy(), np.array([[0, 0.2, 0, 0.5]]))
     assert relu.running_mean is None
     assert relu.running_std is None
 
     # Round 3
-    relu.max_val = 0.15
+    relu.enforced_max = 0.15
     out = relu(input_data[2])
     np.testing.assert_almost_equal(out.numpy(), np.array([0, 0., 0, 0.0]))
     assert relu.running_mean is None
