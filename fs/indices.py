1c0776ba8e1fc21d2934415b04f1230e4ab947a7
brianlan <brianlanbo@gmail.com>
2019-09-04 13:11:35 +0800

    merged 2 get_indices impl, and added random_sample

--- fs/indices.py
+++ fs/indices.py
@@ -1,12 +1,22 @@
 import pathlib
 
+import numpy as np
 
-def get_indices(ind_path, root_dir: pathlib.Path, suffix):
+
+def get_indices(ind_path, root_dir: pathlib.Path, suffix, random_sample=None):
+    """
+    Create file indices list either from indices_path or go through root_dir.
+    :param ind_path: if not None, will return indices from this file, otherwise by traversing root_dir.
+    :param root_dir: file root directory
+    :param suffix: file suffix of target files
+    :param random_sample: (optional) if given, will random sample this number of data and return.
+    :return: a list of path (str)
+    """
     _sfx = suffix.strip(".")
     if ind_path:
         with open(ind_path, "r") as f:
             indices = [l.strip() for l in f.readlines()]
     else:
         indices = sorted([str(p.relative_to(root_dir).with_suffix("")) for p in root_dir.rglob(f"*.{_sfx}")])
-
-    return indices
+    n_samples_to_draw = min(len(indices), random_sample or len(indices))
+    return np.random.choice(indices, size=n_samples_to_draw).tolist()
--- helpers.py
+++ helpers.py
@@ -1,9 +1,13 @@
-def get_indices(root_dir, suffix, indices_path=None):
+import numpy as np
+
+
+def get_indices(root_dir, suffix, indices_path=None, random_sample=None):
     """
     Create file indices list either from indices_path or go through root_dir.
     :param root_dir: file root directory
     :param suffix: file suffix of target files
     :param indices_path: (optional) if given, will return indices from this file, otherwise by traversing root_dir.
+    :param random_sample: (optional) if given, will random sample this number of data and return.
     :return: a list of path (str)
     """
     _sfx = suffix.strip(".")
@@ -12,5 +16,5 @@ def get_indices(root_dir, suffix, indices_path=None):
             indices = [l.strip() for l in f.readlines()]
     else:
         indices = sorted([str(p.relative_to(root_dir).with_suffix("")) for p in root_dir.rglob(f"*.{_sfx}")])
-
-    return indices
+    n_samples = min(len(indices), random_sample or len(indices))
+    return np.random.choice(indices, size=n_samples).tolist()
